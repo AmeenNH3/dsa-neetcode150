57775aab5f59b89dacf68ef7ce28b5abdd55e2b1
AmeenNH3 <md.ameenullah.h@gmail.com>
2022-11-22 23:06:13 +0530

    two sum solved

--- src/com/dsa/arraysandhasing/ContainsDuplicate.java
+++ src/com/dsa/arraysandhasing/ContainsDuplicate.java
@@ -1,38 +1,33 @@
 package com.dsa.arraysandhasing;
 
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class ContainsDuplicate {
     public static void main(String[] args) {
         ContainsDuplicate c = new ContainsDuplicate();
-        boolean result = c.containsDuplicate(new int[]{1, 2,3,1});
+        boolean result = c.containsDuplicate(new int[]{1, 2, 2, 3, 4, 5, 5});
         System.out.println(result);
     }
 
-//
-//    public boolean containsDuplicate(int[] nums){
-//        if (nums.length <= 1) return false;
-//
-//        //Brute Force
-//        //Sorting the array and iterating and counting the elements will take O(nlogn + n)
-//        //Current Solution
-//        //Iterating the array and storing them in hashmap simultaneously looking for duplicates will only take o(n)
-//        HashMap<Integer,Integer> map = new HashMap<>();
-//
-//        for(Integer a : nums){
-//            if(map.containsKey(a)){
-//                map.replace(a,1 + map.get(a));
-//                if(map.get(a) > 1) return true;
-//            }
-//            else{
-//                map.put(a,1);
-//            }
-//        }
-//        return false;
-//    }
-public boolean containsDuplicate(int[] nums) {
-    long count = Arrays.stream(nums).distinct().count();
-    return count == nums.length;
-}
+
+    public boolean containsDuplicate(int[] nums){
+        if (nums.length <= 1) return false;
+
+        //Brute Force
+        //Sorting the array and iterating and counting the elements will take O(nlogn + n)
+        //Current Solution
+        //Iterating the array and storing them in hashmap simultaneously looking for duplicates will only take o(n)
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(Integer a : nums){
+            if(map.containsKey(a)){
+                map.replace(a,1 + map.get(a));
+                if(map.get(a) > 1) return true;
+            }
+            else{
+                map.put(a,1);
+            }
+        }
+        return false;
+    }
 }
---
+++ src/com/dsa/arraysandhasing/TwoSum.java
@@ -0,0 +1,22 @@
+package com.dsa.arraysandhasing;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0;i<nums.length;i++){
+            int reqValue = target - nums[i];
+            if(map.get(reqValue) != null) return  new int[]{map.get(reqValue),i};
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return new int[]{0,0};
+    }
+}
