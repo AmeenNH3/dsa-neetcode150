d8ba7e34322f15b699e5c9ec65f68c65b0d8c719
AmeenNH3 <md.ameenullah.h@gmail.com>
2022-11-25 10:50:03 +0530

    solved

--- src/com/dsa/arraysandhasing/GroupAnagrams.java
+++ src/com/dsa/arraysandhasing/GroupAnagrams.java
@@ -1,61 +1,29 @@
 package com.dsa.arraysandhasing;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class GroupAnagrams {
     public static void main(String[] args) {
-        GroupAnagrams gp  = new GroupAnagrams();
-
+        GroupAnagrams gp = new GroupAnagrams();
         gp.groupAnagrams(new String[] {"eat","ate","ate","tea","tan","ant","eat","","boot","ootb","khan"});
-
     }
     public  List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
 
-//        Arrays.sort(strs);
-        List<List<String>> result = new ArrayList<>();
-        HashMap<String,Boolean> included = new HashMap<>();
-        for(int i =0;i<strs.length;i++){
-            String str  = strs[i];
-            if(included.get(str + "" + i) != null){
-                //do nothing and go to the next element(i++)
-            }
-            else{
-                List<String> temp = new ArrayList<>();
-                temp.add(str);
-                included.put(str + "" + i,true);
-                for(int j = i + 1 ; j< strs.length; j++){
-                    String str2 = strs[j];
-                    if(included.get(str2 + "" + j) != null){
-                        //do nothing and go to the next element(j++)
-                    }
-                    else{
-                        if(isAnagram(str,str2)){
-                            temp.add(str2);
-                            included.put(str2 + "" + j,true);
-                        }
-                    }
-                }
-                result.add(temp);
-            }
+      for (String s : strs) {
+          char[] arr = new  char[26];
+          for( char c : s.toCharArray()){
+              arr[c - 'a']++;
+          }
 
-        }
-        return result;
+          String key = new String(arr);
+
+          map.computeIfAbsent(key, a -> new ArrayList<>());
+          map.get(key).add(s);
+
+      }
+        return new ArrayList<>(map.values());
     }
 
-    private  boolean isAnagram(String str1,String str2){
-        if(str1.length() != str2.length()) return false;
-        int[] arr = new int[26];
-        for(int i =0;i<str1.length();i++){
-            arr[str1.charAt(i) - 97]++;
-            arr[str2.charAt(i) - 97]--;
-        }
-
-        for(Integer i : arr) if(i!=0) return false;
-
-        return true;
-
     }
-}
+
--- src/com/dsa/arraysandhasing/TwoSum.java
+++ src/com/dsa/arraysandhasing/TwoSum.java
@@ -7,9 +7,7 @@ public class TwoSum {
 
     }
     public int[] twoSum(int[] nums, int target) {
-
         HashMap<Integer,Integer> map = new HashMap<>();
-
         for(int i = 0;i<nums.length;i++){
             int reqValue = target - nums[i];
             if(map.get(reqValue) != null) return  new int[]{map.get(reqValue),i};
