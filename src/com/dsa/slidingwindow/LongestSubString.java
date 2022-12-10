519f6c576c34ed939ff072d831a4c01798fb67be
AmeenNH3 <md.ameenullah.h@gmail.com>
2022-12-10 21:37:16 +0530

    solved

--- src/com/dsa/slidingwindow/LongestSubString.java
+++ src/com/dsa/slidingwindow/LongestSubString.java
@@ -5,11 +5,32 @@ import java.util.stream.Collectors;
 
 public class LongestSubString {
     public static void main(String[] args) {
-
+//        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+//        System.out.println(lengthOfLongestSubstring("aab"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
     }
-    public int lengthOfLongestSubstring(String s) {
+    public static int lengthOfLongestSubstring(String s) {
+        //abcabcbb
+        int maxLength = 0;
 
-        return 0;
+        Set<Character> set = new HashSet<>();
+        int i = 0;
+        int j = 0;
+        while( i< s.length()){
+
+            while(set.contains(s.charAt(i))){
+                set.remove(s.charAt(j));
+                j++;
+            }
+            set.add(s.charAt(i));
+
+            maxLength = Math.max(set.size(), maxLength);
+
+            i++;
+        }
+
+
+        return maxLength;
     }
 
 }
