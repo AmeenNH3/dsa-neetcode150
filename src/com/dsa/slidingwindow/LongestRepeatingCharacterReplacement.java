6dd4da7332d6242ca95d2e0bdb55a7d0411686ec
AmeenNH3 <md.ameenullah.h@gmail.com>
2023-01-11 10:12:15 +0530

    added solution 2

--- src/com/dsa/slidingwindow/LongestRepeatingCharacterReplacement.java
+++ src/com/dsa/slidingwindow/LongestRepeatingCharacterReplacement.java
@@ -18,16 +18,11 @@ public class LongestRepeatingCharacterReplacement {
 
         HashMap<Character,Integer> countsMap = new HashMap<>();
         int maxCount = 0;
-        char maxChar;
-
         while( right < s.length()){
-
             Character c = s.charAt(right);
-
             countsMap.put(c,countsMap.getOrDefault(c,0) + 1);
             if(countsMap.get(c) > maxCount) {
                 maxCount = countsMap.get(c);
-                maxChar = c;
             }
 
 
@@ -44,6 +39,32 @@ public class LongestRepeatingCharacterReplacement {
         System.out.println(countsMap);
         return 0;
     }
+    public static int characterReplacement2(String s, int k) {
+        int len = s.length();
+        int[] charCounts = new int[26];
+        int leftPointer = 0;
+        int maxLength = 0;
+        int maxCount = 0;
+        //"ABABBACC", 1
+        for(int rightPointer = 0; rightPointer < len ; rightPointer++){
+            int currentCharCount =  charCounts[s.charAt(rightPointer) - 'A']++;
+            maxCount =  Math.max(currentCharCount,maxCount);
+            //3 - 0 - 2 = 1
+            // Length of the window will always be +1 to rightPointer - leftPointer because we start from 0th
+            //index so for a string say "AMEEN" length is 5, but when you use zero index it will be 4
+            // coz 4(rightPointer) - 0(leftPointer) => 4 which is incorrect and the length is actually 5
+            int windowLength = rightPointer - leftPointer + 1;
+            if(windowLength - maxCount  > k){
+                charCounts[s.charAt(leftPointer) - 'A']--;
+                leftPointer++;
+            }
 
+            maxLength = Math.max(maxLength , windowLength);
+
+
+        }
+
+        return maxLength;
+    }
 
 }
