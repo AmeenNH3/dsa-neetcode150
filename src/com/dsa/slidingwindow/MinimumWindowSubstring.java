e89af9c74a146208d0157f24efc6f3eb1f1a218a
AmeenNH3 <md.ameenullah.h@gmail.com>
2023-02-05 22:51:50 +0530

    initial commit

---
+++ src/com/dsa/slidingwindow/MinimumWindowSubstring.java
@@ -0,0 +1,16 @@
+package com.dsa.slidingwindow;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+public class MinimumWindowSubstring {
+    public static void main(String[] args) {
+//        Input: s = "ADOBECODEBANC", t = "ABC"
+//        Output: "BANC"
+//        Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+        minWindow("ADOBECODEBANC","ABC");
+    }
+    public static String minWindow(String s, String t) {
+        return "";
+    }
+}
